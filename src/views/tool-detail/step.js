9d213336549083f1767b7908d9b647500299f297
dlevine90 <davislevine22@gmail.com>
2016-05-09 16:49:00 +0100

    Tool detail update

--- src/views/tool-detail/step.js
+++ src/views/tool-detail/step.js
@@ -5,7 +5,12 @@ export default class Step extends Component {
 	render() {
 		return (
 			<div>
-			step
+				<div className="row">
+					<div className="small-12 columns">
+						<h4>Step 1</h4>
+						<p>Create a marshmallow challenge kit for each team, with each kit containing 20 of spaghetti, 1 meter of masking tape, 1 meter of string and 1 marshmallow. These ingredients should be placed into a paper lunch bag or envelope, which simplifies distribution and hides the contents, maximizing the element of surprise.</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
