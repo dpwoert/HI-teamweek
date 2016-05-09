e0e8a61de6cb40a556e26273c104e4ef3be1b0f6
davey <davey@dpwoert.com>
2016-05-09 14:16:14 +0100

    made split pane structure in react

---
+++ src/components/filter-viz.js
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class FilterViz extends Component {
+
+	componentDidMount(){
+		const video = ReactDOM.findDOMNode(this);
+	}
+
+	render() {
+
+		return (
+			<div />
+		);
+	}
+}
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,20 @@
 import React, {Component, PropTypes} from 'react';
+import FilterViz from '../../components/filter-viz.js';
+import ToolDetail from '../tool-detail';
 
 export default class Home extends Component {
 
 	render() {
 		return (
-			<div className="home">
-				<span className="home__message">HOME</span>
+			<div className="home__container">
+
+				<div>
+					<FilterViz />
+				</div>
+				<div>
+					<ToolDetail />
+				</div>
+
 			</div>
 		);
 	}
