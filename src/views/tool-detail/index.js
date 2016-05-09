4fd8fdde8ba6603efcd94bf14d98fa48296125b4
dlevine90 <davislevine22@gmail.com>
2016-05-09 15:11:29 +0100

    Add base structure

--- src/views/tool-detail/index.js
+++ src/views/tool-detail/index.js
@@ -1,10 +1,15 @@
 import React, {Component, PropTypes} from 'react';
+import Step from './step.js';
 
 export default class Home extends Component {
 
 	render() {
 		return (
-			<div>detail</div>
+			<div>
+				<Title name={dynamicTitle} />
+				<Short-description />
+				<Step />
+			</div>
 		);
 	}
 }
