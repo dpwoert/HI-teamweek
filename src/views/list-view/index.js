fea5b1414e0c12bb289746c51607af851cc7ebe8
davey <davey@dpwoert.com>
2016-05-10 13:13:39 +0100

    add files for list view

---
+++ src/views/list-view/index.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class ListView extends Component {
+
+	render() {
+
+		return (
+
+				<div className="row">
+				</div>
+
+		);
+	}
+}
